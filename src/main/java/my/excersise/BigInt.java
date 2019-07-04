f09f40d8fbe8bc3ae76b72229fcef51a44281e64
Dobrotvorskiy Dmitriy <dobrotvorskiy.d.i@sberbank.ru>
2019-07-04 10:16:01 +0700

    BigInt multiplication - simple n^2 algorithm is implemented

--- src/main/java/my/excersise/BigInt.java
+++ src/main/java/my/excersise/BigInt.java
@@ -10,14 +10,16 @@ public class BigInt {
 	private final int[] digits; 
 
 	public BigInt(String number) {
-		int x = number.length() / DIGIT_10_MAX_LENGTH;
+		int x = (int) Math.ceil( (double) number.length() / DIGIT_10_MAX_LENGTH);
 		int size = nearestPow2(x);
 		digits = new int[size];
+		// System.out.println("x="+x+" size="+size+" DIGIT_10_MAX_LENGTH="+DIGIT_10_MAX_LENGTH);
 
 		int endIndex = number.length();
 		int idx = 0;
 		do {
 			int beginIndex = endIndex > DIGIT_10_MAX_LENGTH ? (endIndex - DIGIT_10_MAX_LENGTH) : 0;
+			// System.out.println("beginIndex="+beginIndex+" endIndex="+endIndex + "idx="+idx);
 			digits[ size - (++idx) ] = Integer.parseInt( number.substring(beginIndex, endIndex));
 			endIndex -= DIGIT_10_MAX_LENGTH;
 		} while (endIndex > 0);
@@ -27,16 +29,18 @@ public class BigInt {
 	}
 
 	static int nearestPow2(int x) {
+		if (x < 0) throw new IllegalArgumentException("only positive numbers are allowed");
+		if (x < 2) return 2;
+
 		int res = Integer.highestOneBit(x);
 		res = x > res ? res << 1 : res;
     	return res;
-    	// return 2 << ( (int) Math.floor( (Math.log(x) / Math.log(2)))  );
 	}
 
 	public BigInt add(BigInt b) {
 		int radix = (int) Math.pow(10, DIGIT_10_MAX_LENGTH);
 
-		int[] buff = new int[ Math.max(b.digits.length, digits.length) ];
+		int[] buff = new int[ Integer.max(b.digits.length, digits.length) ];
 		int addition = 0;
 		for (int i = 0; i < buff.length; i++) {
 			int x = i >= digits.length ? 0 : digits[digits.length - i - 1];
@@ -54,6 +58,42 @@ public class BigInt {
 			newDigits = buff;
 		}
 		return new BigInt(newDigits);
+	}
+
+	public BigInt multiply(BigInt b) {
+		int maxValue = (int) Math.pow(10, DIGIT_10_MAX_LENGTH);
+		int n = Integer.max(b.digits.length, digits.length);
+		int[] buff = new int[ Integer.max(b.digits.length, digits.length) << 1 ];
+		int addition = 0;			
+		for (int i = 0; i < digits.length; i++) {
+			int x = digits[digits.length - i - 1];
+			for (int j = 0; j < b.digits.length; j++ ) {
+				int y = b.digits[b.digits.length - j - 1];
+				int sum = addition + x * y + buff[buff.length - (i + j) - 1];
+				buff[buff.length - (i + j) - 1] = sum % maxValue;
+				addition = sum / maxValue;
+			}
+		}
+		// expand resulting array if necessary
+		if (addition > 0) {
+			int[] buff1 = new int[buff.length << 1];
+			System.arraycopy(buff, 0, buff1, buff.length, buff.length);
+			buff1[buff.length] = addition;
+			return new BigInt(buff1);
+		}
+
+		// shink resulting array if necessary
+		int highestDigitIdx = -1;
+		while (++highestDigitIdx < buff.length && buff[highestDigitIdx] == 0) {};
+		int expectedSize = nearestPow2(buff.length - highestDigitIdx);
+		System.out.println("highestDigitIdx="+highestDigitIdx+" buff="+Arrays.toString(buff)+ " expectedSize=" + expectedSize);
+		if (buff.length > expectedSize) {
+			int[] buff1 = new int[expectedSize];
+			System.arraycopy(buff, buff.length - expectedSize, buff1, 0, expectedSize);
+			System.out.println("buff1="+Arrays.toString(buff1));
+			return new BigInt(buff1);
+		}
+		return new BigInt(buff);
 	}
 
 	public String toDebugString() {
--- src/test/java/my/excersise/BigIntTest.java
+++ src/test/java/my/excersise/BigIntTest.java
@@ -11,6 +11,7 @@ public class BigIntTest {
 			new Object[] {"1"},
 			new Object[] {"10"},
 			new Object[] {"1000"},
+			new Object[] {"10000000"},
 			new Object[] {"123123123"},
 			new Object[] {"1111111111222222222233333333334444444444555555555566666666667777777777888888888899999999990000000000"}
 		};
@@ -19,7 +20,7 @@ public class BigIntTest {
 	@Test(dataProvider = "test_data")
 	public void test_constructor(String number){
 
-		System.out.println("number= " + number);
+		System.out.println("number=" + number);
 		BigInt b = new BigInt(number);
 		System.out.println(b.toString());		
 		System.out.println(b.toDebugString());
@@ -55,6 +56,33 @@ public class BigIntTest {
 		assertEquals(s1.toString(), sum);
 	}
 
+	@DataProvider
+	public Object[][] test_multiply_data() {
+		return new Object[][] {
+			new Object[] {"0", "0", "0" },
+			new Object[] {"0", "1", "0" },
+			new Object[] {"0", "100000000000", "0" },
+			new Object[] {"1", "9", "9" },
+			new Object[] {"100", "900", "90000" },
+			new Object[] {"1000", "1000", "1000000"},
+			new Object[] {"90000000", "20000000", "1800000000000000"},
+			new Object[] {"1234", "5678", "7006652" }
+		};
+	}
+
+	@Test(dataProvider = "test_multiply_data")
+	public void test_multiply(String x, String y, String prod){
+
+		BigInt a = new BigInt(x);
+		BigInt b = new BigInt(y);
+		BigInt s1 = a.multiply(b);
+		BigInt s2 = b.multiply(a);
+
+		
+		assertEquals(s1, s2);
+		assertEquals(s1.toString(), prod, "Expected " + x + " * " + y + " = " + prod);
+	}
+
     @DataProvider
 	public Object[][] test_nearestPow2_data() {
 		return new Object[][] {
@@ -67,11 +95,13 @@ public class BigIntTest {
 			new Object[] {8, 8},
 			new Object[] {9, 16},
 			new Object[] {16, 16},
-			new Object[] {17, 32}
+			new Object[] {17, 32},
+			new Object[] {Integer.MAX_VALUE, Integer.MIN_VALUE}
 		};
 	}
 	@Test(dataProvider = "test_nearestPow2_data")
 	public void test_nearestPow2(int x, int res) {
 		assertEquals(BigInt.nearestPow2(x), res);
 	}
+
 }
